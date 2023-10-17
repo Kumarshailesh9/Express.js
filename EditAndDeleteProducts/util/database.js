b11410ba74c959d1873d6dc9de02bb23e339be62
Kumarshailesh9 <theshaileshkumar9@gmail.com>
2023-10-17 20:45:28 +0530

    Retrieving data from DB And Delete functionality

--- EditAndDeleteProducts/util/database.js
+++ EditAndDeleteProducts/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'sys',
-    password: 'shailesh'
+    password: 'Realme@333'
 });
 
 module.exports = pool.promise();
